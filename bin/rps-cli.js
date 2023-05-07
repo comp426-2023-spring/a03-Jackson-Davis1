ee44cd9befc28f07e9731d659392d1c3121a9203
Jackson <jdavis19@ad.unc.edu>
2023-05-07 00:47:33 -0400

    command line args

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -2,10 +2,55 @@
 
 // Your CLI code here
 import minimist from 'minimist';
-const argv = minimist(process.argv.slice(2));
+import { rps } from "../lib/rpsls.js"
 
+var args = minimist(process.argv.slice(2));
 
-console.log(argv);
-const playableOptions = ["rock", "paper", "scissors", "lizard", "spock"]
-let opponentShot = playableOptions[Math.floor(Math.random()*3)]
-console.log(opponentShot);
+if (args.h || args.help) { // If someone asks for help, print message 
+	console.log(
+  `Usage: node-rps [SHOT]
+  Play Rock Paper Scissors (RPS)
+  
+    -h, --help      display this help message and exit
+    -r, --rules     display the rules and exit
+  
+  Examples:
+    node-rps        Return JSON with single player RPS result.
+                    e.g. {"player":"rock"}
+    node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+
+	process.exit(0);
+}
+
+if (args.r || args.rules) { 
+	console.log(
+  `Rules for Rock Paper Scissors:
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock CRUSHES Scissors`);
+
+	process.exit(0);
+}
+
+let shot = args._[0] 
+
+try {
+	console.log(JSON.stringify(rps(shot))); 
+} catch (e) {
+	console.log(`Usage: node-rps [SHOT]
+  Play Rock Paper Scissors (RPS)
+  
+    -h, --help      display this help message and exit
+    -r, --rules     display the rules and exit
+  
+  Examples:
+    node-rps        Return JSON with single player RPS result.
+                    e.g. {"player":"rock"}
+    node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                    e.g {"player":"rock","opponent":"scissors","result":"win"}`); // Show em how to play if they mess up
+	console.log(`Rules for Rock Paper Scissors:
+  - Scissors CUTS Paper
+  - Paper COVERS Rock
+  - Rock CRUSHES Scissors`);
+}      
