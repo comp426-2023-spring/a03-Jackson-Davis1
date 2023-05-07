92813907e8c04ba1924bb2d0bc397d847c68129f
Jackson <jdavis19@ad.unc.edu>
2023-05-07 01:27:32 -0400

    final

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -6,7 +6,7 @@ import { rps } from "../lib/rpsls.js"
 
 var args = minimist(process.argv.slice(2));
 
-if (args.h || args.help) { // If someone asks for help, print message 
+if (args.h || args.help) { 
 	console.log(
   `Usage: node-rps [SHOT]
   Play Rock Paper Scissors (RPS)
@@ -48,7 +48,7 @@ try {
     node-rps        Return JSON with single player RPS result.
                     e.g. {"player":"rock"}
     node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                    e.g {"player":"rock","opponent":"scissors","result":"win"}`); // Show em how to play if they mess up
+                    e.g {"player":"rock","opponent":"scissors","result":"win"}`); 
 	console.log(`Rules for Rock Paper Scissors:
   - Scissors CUTS Paper
   - Paper COVERS Rock
